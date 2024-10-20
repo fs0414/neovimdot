f89eff06c472a2ad5d97a27e8d5960edc5f5502b
fujitani sora <fujitanisora0414@gmail.com>
2024-10-21 01:43:38 +0900

    gittools amend

--- init.lua
+++ init.lua
@@ -3,6 +3,20 @@ vim.g["fern#show_hidden"] = 1
 
 vim.opt.runtimepath:append("~/lua/extension/ts-trycatch-encoder/main.ts")
 
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   pattern = "*",
+--   callback = function()
+--     vim.cmd("TSEnable highlight")
+--   end,
+-- })
+
+-- require'nvim-treesitter.configs'.setup {
+--   ensure_installed = "maintained",
+--   highlight = {
+--     enable = true,
+--   },
+-- }
+
 require('plugin')
 require('base')
 require('keymap')
@@ -14,3 +28,4 @@ require('plugin/neotest-config')
 require('plugin/dap-config')
 require('plugin/gittool')
 require('plugin/notice-config')
+require('plugin/nvim-web-devicons')
--- lua/plugin/gittool.lua
+++ lua/plugin/gittool.lua
@@ -13,14 +13,11 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- lazygit
-map('n', '<leader>g', ':Lazygit<CR>', opts)
-
--- diffview
 require("diffview").setup()
-map('n', '<leader>df', ':DiffviewOpen<CR>', opts)
-
--- neogit
 local neogit = require('neogit')
 neogit.setup {}
 require("diffview").setup()
 
+map('n', '<leader>df', ':DiffviewOpen<CR>', opts)
+map('n', '<leader>g', ':Lazygit<CR>', opts)
+map('n', '<leader>ng', ':Neogit<CR>', opts)
